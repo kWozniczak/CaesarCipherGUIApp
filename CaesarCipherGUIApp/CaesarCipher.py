af7f9adb454f6806c340402485e7edb3a7557edb
kWozniczak <wozniczak.karolina1997@gmail.com>
2023-12-01 14:52:22 +0100

    Add main frame and custom colors

--- CaesarCipherGUIApp/CaesarCipher.py
+++ CaesarCipherGUIApp/CaesarCipher.py
@@ -1,1 +1,22 @@
+import tkinter as tk
+import sys
 
+class CaesarCipher(tk.Frame):
+    def __init__(self, root):
+        self.color1 = "#B5A8B8"
+        self.color2 = "#FEF7FF"
+        self.color3 = "#807483"
+
+        super().__init__(root, bg=self.color1)
+        
+        self.main_frame = self
+        self.main_frame.pack(fill=tk.BOTH, expand=True)
+        self.main_frame.columnconfigure(0, weight=1)
+        
+
+root = tk.Tk()
+caesar_cipher_app = CaesarCipher(root)
+root.title("Caesar Cipher")
+root.geometry('800x500')
+root.resizable(width=False, height=False)
+root.mainloop()
